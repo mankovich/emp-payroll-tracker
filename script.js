447cff60cd7e1518733af8b3fe8f87e75055ad15
Joey M <172998670+mankovich@users.noreply.github.com>
2024-07-01 12:51:26 -0400

    completed code

--- script.js
+++ script.js
@@ -4,29 +4,24 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 
 const collectEmployees = function () {
-  let firstName = prompt("Enter employee's first name:")
-  let lastName = prompt("Enter employee's last name:")
-  let salary = prompt("Enter employee's salary:")
-  if (isNaN(salary)) {salary = "0"}
-  return { firstName, lastName, salary }
+  let employeesArray = []
+  let cont = true
+  while (cont === true) {
+    let firstName = prompt("Enter employee's first name:")
+    let lastName = prompt("Enter employee's last name:")
+    let salary = parseFloat(prompt("Enter employee's salary:")) 
+    if (isNaN(salary)) {salary = 0}
+    let empData = { firstName, lastName, salary } 
+    employeesArray.push(empData)
+    cont = confirm('Do you want to add another employee?')
+  } 
+  return employeesArray
 }
-  
-let employeesArray = []
-console.log(employeesArray)
-
-let cont = true
-while (cont === true) {
-  let newEmployee = collectEmployees()
-  employeesArray.push(newEmployee)
-  cont = confirm('Do you want to add another employee?')
-} 
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
-  
-  let sumSalary = FIXME:;
-  // let initialValue = 0  
-  // let sumSalary = employeesArray.reduce((accumulator, employeesArray) => accumulator + employeesArray.salary, initialValue,)
+  let initialValue = 0  
+  let sumSalary = employeesArray.reduce((accumulator, employeesArray) => accumulator + employeesArray.salary, initialValue,)
   let numEmps = employeesArray.length
   let avgS = sumSalary/numEmps
     console.log(`The average employee salary among our ${numEmps} employee(s) is ${avgS}.`)
@@ -40,11 +35,6 @@ const getRandomEmployee = function(employeesArray) {
   let r = (Math.floor(Math.random() * (totalEmps)) - 1)
   console.log(r)
   let randomEmployee = employeesArray.at(r)
-
-  console.log(employeesArray.at(r))
-
-  console.log(randomEmployee)
-
   console.log(`Congratulations to ${randomEmployee.firstName} ${randomEmployee.lastName}, our random drawing winner!`)
 }
 
